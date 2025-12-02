9f50be13a6ded3f1952424d5684bef26a878362f
0786abu <abubakar77a9@gmail.com>
2025-12-02 15:41:53 +0500

    change

--- src/app/(dashboard)/dashboard/create-property/page.jsx
+++ src/app/(dashboard)/dashboard/create-property/page.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 import FooterThree from "@/layout/footers/FooterThree";
 import Breadcrumb from "@/layout/Breadcrumb/Breadcrumb";
 import dynamic from "next/dynamic";
-const BodyContent = dynamic(() => import("../../../../../components/pages/userPanel"), {
+const BodyContent = dynamic(() => import("@/components/pages/userPanel"), {
   ssr: false,
 });
 
