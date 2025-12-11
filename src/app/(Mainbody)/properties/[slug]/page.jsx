aeaeff1892dd66c8c94caeb7fa060641ba33f0ba
0786abu <abubakar77a9@gmail.com>
2025-12-11 18:13:03 +0500

    change

--- src/app/(Mainbody)/properties/[slug]/page.jsx
+++ src/app/(Mainbody)/properties/[slug]/page.jsx
@@ -3,6 +3,16 @@ import BodyContent from "@/components/property/stickyTabOrClassic";
 import FooterThree from "@/layout/footers/FooterThree";
 
 
+export async function generateStaticParams() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/property/create-property`, {
+    cache: "force-cache", // ✅ recommended for SSG
+  });
+  const {properties} = await res.json();
+
+  return properties?.map((property) => ({
+    slug: property.slug,
+  }));
+}
 const page = async({params}) => {
     const {slug} = await params;
     const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/property/create-property/${slug}`,{ method:"GET", cache:"no-cache" });
