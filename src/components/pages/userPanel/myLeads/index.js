ca39542e28c7c5cf4c41eb4ba1451ca5e8215d91
0786abu <abubakar77a9@gmail.com>
2025-12-16 22:50:25 +0500

    change

--- src/components/pages/userPanel/myLeads/index.js
+++ src/components/pages/userPanel/myLeads/index.js
@@ -42,7 +42,7 @@ const MyLeads = () => {
                         </ModalBody>
                 
                       </Modal>
-        <h5 className="mb-3 fw-semibold">Leads ({myLeads?.length>1 ? (myLeads?.length) : 0})</h5>
+        <h5 className="mb-3 fw-semibold">Leads ({myLeads?.length>0 ? (myLeads?.length) : 0})</h5>
         {leadloading ? (<ProfileLoader/>) : (
             <Table hover bordered responsive className="align-middle mb-0">
           <thead className="table-light sticky-top">
