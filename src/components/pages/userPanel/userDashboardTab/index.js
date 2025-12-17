187a7bfbfd58be697fd27753f25abcb669c78f52
0786abu <abubakar77a9@gmail.com>
2025-12-17 16:35:23 +0500

    change

--- src/components/pages/userPanel/userDashboardTab/index.js
+++ src/components/pages/userPanel/userDashboardTab/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Row } from "reactstrap";
 import AvailableProperty from "./AvailableProperty";
 import PropertyOverview from "./PropertyOverview";
