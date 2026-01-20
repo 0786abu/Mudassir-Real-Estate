3cc854302ad35dbe8f96a24a458105d27ad21991
0786abu <abubakar77a9@gmail.com>
2026-01-20 17:06:53 +0500

    change

--- src/app/(dashboard)/dashboard/agent-dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/agent-dashboard/page.jsx
@@ -1,11 +1,16 @@
 "use client";
 import React, { Fragment } from "react";
-import FooterThree from "@/layout/footers/FooterThree";
 import Breadcrumb from "@/layout/Breadcrumb/Breadcrumb";
 import dynamic from "next/dynamic";
 const BodyContent = dynamic(() => import("../../../../components/pages/userPanel"), {
   ssr: false,
 });
+export const metadata = {
+  title: "Agent Dashboard | Pak Earth",
+  description:
+    "Agent Dashboard on Pak Earth where real estate agents can manage property listings, view inquiries, track performance, and handle client communications efficiently.",
+};
+
 
 const UserDashboard = () => {
   return (
