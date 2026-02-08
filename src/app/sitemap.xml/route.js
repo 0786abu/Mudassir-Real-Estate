5a2e50c588941c09a0afef406bd892e79f51fe68
0786abu <abubakar77a9@gmail.com>
2026-02-08 18:31:01 +0500

    change

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -30,6 +30,7 @@ export async function GET() {
     "/hot-properties",
     "/properties",
     "/projects",
+    "/agents",
   ];
 
   let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
